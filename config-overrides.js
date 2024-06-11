726974e4770bc0bff2c4299bea55335f783cc9f7
Mustafa Naqvi <mustafa-naqvi@outlook.com>
2024-06-11 20:16:54 +0500

    first project commit

---
+++ config-overrides.js
@@ -0,0 +1,31 @@
+const webpack = require("webpack");
+module.exports = function override(webpackConfig) {
+    webpackConfig.resolve.fallback = {
+        url: require.resolve("url"),
+        querystring: false,
+        assert: require.resolve("assert"),
+        crypto: require.resolve("crypto-browserify"),
+        path: require.resolve("path"),
+        fs: false,
+        os: false,
+        http: require.resolve("stream-http"),
+        https: require.resolve("https-browserify"),
+        buffer: require.resolve("buffer"),
+        stream: require.resolve("stream-browserify"),
+        // zlib: require.resolve("browserify-zlib"),
+    };
+    /* webpackConfig.resolve.alias = {
+      ...webpackConfig.resolve.alias,
+      "@hashgraph/proto": "@hashgraph/proto/lib/proto.js",
+    };*/
+    webpackConfig.plugins.push(
+        new webpack.ProvidePlugin({
+            process: "process/browser",
+            Buffer: ["buffer", "Buffer"],
+
+        }),
+    );
+    //webpackConfig.devServer.https = true;
+
+    return webpackConfig;
+};
